a1bc798f19346d26987e7226eeb8b8940585d852
Christoph Potzinger <criscom27@gmail.com>
2013-08-18 15:23:42 +0200

    Some media queries; adjusting main nav to mobile devices and slideshow/royalslider to mobile

--- js/touchcarousel.js
+++ js/touchcarousel.js
@@ -1,29 +1,9 @@
 (function ($) {
   $(document).ready(function() {
        
-    //Touchcarousel Media Element Homepage in branding zone, ganz unten
-   $("#HPmedia").touchCarousel({
+    //Frontpage Image Rotator gemeindegabersdrof.
+   $("#frontpagerotator").touchCarousel({
            /* carousel options go here see Javascript Options section for more info */			
        });
-       
-   //Touchcarousel Featured Products Element Homepage
-   $("#HPfeaturedproducts").touchCarousel({
-           /* carousel options go here see Javascript Options section for more info */			
-       });
-   // Touchcarousel Reports & Insights Detail bottom
-   $("#browsepublications").touchCarousel({
-           /* carousel options go here see Javascript Options section for more info */			
-       });
-       
-   // Touchcarousel BM Issue - Browse Articles
-   $("#browsearticles").touchCarousel({
-           /* carousel options go here see Javascript Options section for more info */			
-       });
-       
-   // Touchcarousel BM Issue - Browse Articles
-   $("#relatedcontentbooks").touchCarousel({
-           /* carousel options go here see Javascript Options section for more info */			
-       });
-	    
    });
 })(jQuery);
---
+++ templates/views-view--image-rotator-frontpage--block.tpl.php
@@ -0,0 +1,7 @@
+<div class="<?php print $classes; ?>">
+<div id="frontpagerotator" class="touchcarousel black-and-white">
+  <?php if ($rows) { ?>
+      <?php print $rows; ?>
+  <?php } ?>
+</div> <!-- //HProtator -->
+</div> <!-- //classes -->
